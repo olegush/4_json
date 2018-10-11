93bb9e255ede11db1d66862341537f0b0dff98ba
U-Люба-ПК\Люба <oleg107117@gmail.com>
2018-10-11 11:14:52 +0300

    encoding bug and var names is fixed, checking fo file exists and correct json data added

--- pprint_json.py
+++ pprint_json.py
@@ -1,16 +1,25 @@
 import json
+import os.path
 
 
 def load_data(filepath):
-    with open(filepath, 'rb') as file:
-        data_json = json.loads(file.read())
-    return data_json
+    if os.path.exists(filepath):
+        with open(filepath, 'rb') as file:
+            try:
+                data_json = json.loads(file.read())
+            except ValueError:
+                return "json data does not exists or incorrect"
+            else:
+                return data_json
+    else:
+        return "file does not exists"
 
 
-def pretty_print_json(user_data):
-    return json.dumps(user_data, sort_keys=True, indent=4)
+def pretty_print_json(unformatted_json):
+    return json.dumps(unformatted_json,
+                      sort_keys=True, indent=4, ensure_ascii=False,)
 
 
 if __name__ == '__main__':
-    user_data_obj = load_data('data.json')
-    print(pretty_print_json(user_data_obj))
+    user_unformatted_json = load_data('data.json')
+    print(pretty_print_json(user_unformatted_json))
