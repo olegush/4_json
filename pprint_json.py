d936c5b1c98cc8c73b3470258469d735a1eb695c
olegush <43958767+olegush@users.noreply.github.com>
2018-10-13 11:06:13 +0300

    fixed bugs and corrected the code
    added command line argument, fixed var names, formatted style code, simplified function

--- pprint_json.py
+++ pprint_json.py
@@ -1,25 +1,29 @@
 import json
 import os.path
+import sys
 
 
 def load_data(filepath):
     if os.path.exists(filepath):
         with open(filepath, 'rb') as file:
             try:
-                data_json = json.loads(file.read())
+                data_list = json.loads(file.read())
+                return data_list
             except ValueError:
                 return "json data does not exists or incorrect"
-            else:
-                return data_json
     else:
-        return "file does not exists"
+        return None
 
 
 def pretty_print_json(unformatted_json):
-    return json.dumps(unformatted_json,
-                      sort_keys=True, indent=4, ensure_ascii=False,)
+    return json.dumps(
+        unformatted_json,
+        sort_keys=True,
+        indent=4,
+        ensure_ascii=False,
+    )
 
 
 if __name__ == '__main__':
-    user_unformatted_json = load_data('data.json')
-    print(pretty_print_json(user_unformatted_json))
+    user_unformatted_data_list = load_data(sys.argv[1])
+    print(pretty_print_json(user_unformatted_data_list))
