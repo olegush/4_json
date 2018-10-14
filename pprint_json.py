da98a95988820ecf6d438a1ac2369f97fdea660d
olegush <43958767+olegush@users.noreply.github.com>
2018-10-14 11:53:39 +0300

    added error messeges

--- pprint_json.py
+++ pprint_json.py
@@ -9,7 +9,7 @@ def load_data(filepath):
             decoded_json = json.loads(file.read())
             return decoded_json
         except ValueError:
-            pass
+            print ('need json format')
 
 
 def pretty_print_json(decoded_json):
@@ -22,8 +22,12 @@ def pretty_print_json(decoded_json):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
+    if len(sys.argv) == 2:
         filepath = sys.argv[1]
         if os.path.exists(filepath):
             user_decoded_json = load_data(filepath)
             print(pretty_print_json(user_decoded_json))
+        else:
+            print('need correct filepath')
+    else:
+        print ('need filename')
