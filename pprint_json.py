6dbbca016f5d2e9542960940ed703b62ef87448e
olegush <43958767+olegush@users.noreply.github.com>
2018-10-14 19:52:08 +0300

    cleaned functions

--- pprint_json.py
+++ pprint_json.py
@@ -3,13 +3,21 @@ import os.path
 import sys
 
 
-def load_data(filepath):
+def load_data(filepath, error):
     with open(filepath, 'rb') as file:
         try:
-            decoded_json = json.loads(file.read())
+            read_data = file.read()
+            return read_data
+        except ValueError:
+            print(error)
+
+
+def decode_json(data_to_decode, error):
+        try:
+            decoded_json = json.loads(data_to_decode)
             return decoded_json
         except ValueError:
-            print ('need json format')
+            print(error)
 
 
 def pretty_print_json(decoded_json):
@@ -23,11 +31,12 @@ def pretty_print_json(decoded_json):
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        filepath = sys.argv[1]
-        if os.path.exists(filepath):
-            user_decoded_json = load_data(filepath)
+        user_filepath = sys.argv[1]
+        if os.path.exists(user_filepath):
+            user_read_data = load_data(user_filepath, 'need json format')
+            user_decoded_json = decode_json(user_read_data, 'need json format')
             print(pretty_print_json(user_decoded_json))
         else:
-            print('need correct filepath')
+            print('need correct file path')
     else:
-        print ('need filename')
+        print('need filename')
