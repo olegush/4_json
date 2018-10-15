a19b653c83bd8f3a3ed6bda69338c2391fbe1b1c
olegush <43958767+olegush@users.noreply.github.com>
2018-10-15 09:03:55 +0300

    improved functions

--- pprint_json.py
+++ pprint_json.py
@@ -6,36 +6,30 @@ import sys
 def load_data(filepath, error):
     with open(filepath, 'rb') as file:
         try:
-            read_data = file.read()
-            return read_data
+            return file.read()
         except ValueError:
             print(error)
 
 
-def decode_json(data_to_decode, error):
-        try:
-            decoded_json = json.loads(data_to_decode)
-            return decoded_json
-        except ValueError:
-            print(error)
-
-
-def pretty_print_json(decoded_json):
-    return json.dumps(
-        decoded_json,
-        sort_keys=True,
-        indent=4,
-        ensure_ascii=False,
-    )
+def pretty_print_json(data_to_decode, error):
+    try:
+        decoded_json = json.loads(data_to_decode)
+        return json.dumps(
+            decoded_json,
+            sort_keys=True,
+            indent=4,
+            ensure_ascii=False,
+        )
+    except ValueError:
+        print(error)
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         user_filepath = sys.argv[1]
         if os.path.exists(user_filepath):
-            user_read_data = load_data(user_filepath, 'need json format')
-            user_decoded_json = decode_json(user_read_data, 'need json format')
-            print(pretty_print_json(user_decoded_json))
+            user_read_data = load_data(user_filepath, 'file does not reading')
+            print(pretty_print_json(user_read_data, 'need json format'))
         else:
             print('need correct file path')
     else:
