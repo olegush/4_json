32f28f40b7bb270b7a42a3910b23ed018d0ab89c
olegush <43958767+olegush@users.noreply.github.com>
2018-10-16 08:46:51 +0300

    corrected if statements

--- pprint_json.py
+++ pprint_json.py
@@ -20,13 +20,10 @@ def pretty_print_json(decoded_json):
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         exit('need filename')
-    else:
-        user_filepath = sys.argv[1]
-        if not os.path.exists(user_filepath):
-            exit('need correct file path')
-        else:
-            loaded_json = load_data(user_filepath)
-            if not loaded_json:
-                exit('can not load the json')
-            else:
-                print(pretty_print_json(loaded_json))
+    user_filepath = sys.argv[1]
+    if not os.path.exists(user_filepath):
+        exit('need correct file path')
+    loaded_json = load_data(user_filepath)
+    if not loaded_json:
+        exit('can not load the json')
+    print(pretty_print_json(loaded_json))
